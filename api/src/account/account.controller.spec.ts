5c81eff4a22714abfe1e04c718ade043be870de3
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-04-11 10:01:22 +0300

    Make integration test on AccountController

--- api/src/account/account.controller.spec.ts
+++ api/src/account/account.controller.spec.ts
@@ -2,30 +2,68 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AccountController } from './account.controller';
 import { AccountService } from './account.service';
 import { JwtService } from '@nestjs/jwt';
+import { INestApplication } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Account } from './account.entity';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+import { RoleService } from 'src/role/role.service';
+import { Role } from 'src/role/role.entity';
+import request from "supertest";
+import { AccountDto } from './account.dto';
 
 describe('UserController', () => {
-    let controller: AccountController;
+    let app: INestApplication;
+    let repository: Repository<Role>;
 
-    beforeEach(async () => {
+    beforeAll(async () => {
+        process.env.JWT_SECRET="loremipsumdolorsitametconsecteturadipiscingelitseddoeiusmodtempx";
+        process.env.PASSWORD_STRENGTH="12";
         const module: TestingModule = await Test.createTestingModule({
+            imports: [
+                TypeOrmModule.forRoot({
+                    type: "sqlite",
+                    database: ":memory:",
+                    entities: [Account, Role],
+                    synchronize: true
+                }),
+                TypeOrmModule.forFeature([Account, Role])
+            ],
             controllers: [AccountController],
-            providers: [{
-                provide: AccountService,
-                useValue: {
-                    createUser: jest.fn()
-                }
-            }, {
-                provide: JwtService,
-                useValue: {
-                    verifyAsync: jest.fn()
-                }
-            }]
+            providers: [AccountService, RoleService, JwtService]
         }).compile();
 
-        controller = module.get<AccountController>(AccountController);
+        app = module.createNestApplication();
+        app.init();
+        repository = module.get<Repository<Role>>(getRepositoryToken(Role));
+    });
+
+    afterAll(async () => {
+        await app.close();
+    });
+
+    beforeEach(async () => {
+        await repository.clear();
+        await repository.save([
+            { id: 1, name: "Client" }
+        ]);
     });
 
     it('should be defined', () => {
-        expect(controller).toBeDefined();
+        expect(repository).toBeDefined();
+    });
+
+    it("should return Account", async () => {
+        const newAccount: AccountDto = {
+            username: "johndoe",
+            password: "pwd123"
+        };
+
+        const res = await request(app.getHttpServer())
+            .post("/api/account/create")
+            .send(newAccount);
+
+        expect(res.status).toBe(201);
+        expect(res.body).toBeDefined();
+        expect(res.body.username).toBe(newAccount.username);
     });
 });
