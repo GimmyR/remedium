48735d1eb9a4aad1c25cab88de11b89b376c6bfe
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-04-13 16:40:38 +0300

    Add configuration and setup for Jest

---
+++ front/jest.config.ts
@@ -0,0 +1,13 @@
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+    dir: "./"
+});
+
+export default createJestConfig({
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+    testEnvironment: "jest-environment-jsdom",
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/$1',
+    }
+});
