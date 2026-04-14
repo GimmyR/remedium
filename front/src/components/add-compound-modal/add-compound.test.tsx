393b4b0502fb408708db5e352b2c30e8ab9890ad
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-04-14 09:21:18 +0300

    Make unit test on AddCompound component in one test

--- front/src/components/add-compound-modal/add-compound.test.tsx
+++ front/src/components/add-compound-modal/add-compound.test.tsx
@@ -12,21 +12,13 @@ const props = {
 };
 
 describe("Test AddCompound component", () => {
-    it("should display title", () => {
+    it("should display title, unit and add button", () => {
         render(<AddCompound {...props}/>);
         const title = screen.getByText(props.compound.title);
-        expect(title).toBeDefined();
-    });
-
-    it("should display unit in parentheses", () => {
-        render(<AddCompound {...props}/>);
         const unit = screen.getByText(`(${props.compound.unit})`);
-        expect(unit).toBeDefined();
-    });
-
-    it("should display add button", () => {
-        render(<AddCompound {...props}/>);
         const button = screen.getByRole("button");
+        expect(title).toBeDefined();
+        expect(unit).toBeDefined();
         expect(button).toHaveTextContent("Add");
     });
 });
