9a4ffc50b73174ad66fbefb3a090b2bb3b938659
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-04-24 09:36:10 +0300

    Search elements with 'link' role

--- front/src/components/header/header.test.tsx
+++ front/src/components/header/header.test.tsx
@@ -4,13 +4,13 @@ import Header from ".";
 describe("Test Header component", () => {
     it("should display Remedium", () => {
         render(<Header/>);
-        const brand = screen.getByText("Remedium");
+        const brand = screen.getByRole("link", { name: /remedium/i });
         expect(brand).toBeInTheDocument();
     });
 
     it("should display sign in button", () => {
         render(<Header/>);
-        const button = screen.getByRole("button");
-        expect(button).toHaveTextContent("Sign in");
+        const button = screen.getByRole("link", { name: /sign in/i });
+        expect(button).toBeInTheDocument();
     });
 });
