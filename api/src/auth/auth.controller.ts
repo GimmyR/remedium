b3c818313653b4a136d05aa7c92350d2d083c526
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-04-28 10:34:48 +0300

    Create GET endpoint to check if authenticated user is admin and test it

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,9 +1,13 @@
-import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AccountDto } from 'src/account/account.dto';
+import { AuthGuard } from './auth.guard';
+import { RolesGuard } from './roles.guard';
+import { Roles } from './roles.decorator';
 
 @ApiTags('auth')
+@ApiBearerAuth('access-token')
 @Controller('api/auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
@@ -16,5 +20,16 @@ export class AuthController {
         return {
             access_token: await this.authService.login(user),
         };
+    }
+
+    @UseGuards(AuthGuard, RolesGuard)
+    @Roles('Admin')
+    @Get("is-admin")
+    @ApiOperation({ summary: "Check if authenticated user is admin" })
+    @ApiResponse({ status: HttpStatus.OK, description: "Authenticated user is admin" })
+    @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: "There is no authenticated user" })
+    @ApiResponse({ status: HttpStatus.FORBIDDEN, description: "Authenticated user is not admin" })
+    async isAdmin() {
+        return { isAdmin: true };
     }
 }
