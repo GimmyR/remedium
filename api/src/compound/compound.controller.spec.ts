d71c1fb799a58eabf8f7c521549c9353d47d0045
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-05-30 08:47:57 +0300

    Add script to test /api/compounds/ POST endpoint

--- api/src/compound/compound.controller.spec.ts
+++ api/src/compound/compound.controller.spec.ts
@@ -7,12 +7,19 @@ import { Repository } from 'typeorm';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 describe('CompoundController', () => {
     let app: INestApplication;
     let repository: Repository<Compound>;
+    let jwtService: JwtService;
+    let mockToken: string;
 
     beforeAll(async () => {
+        process.env.JWT_SECRET = 'loremipsumdolorsitametconsecteturadipiscingelitseddoeiusmodtempx';
+        process.env.PASSWORD_STRENGTH = '12';
+
         const module: TestingModule = await Test.createTestingModule({
             imports: [
                 TypeOrmModule.forRoot({
@@ -22,6 +29,14 @@ describe('CompoundController', () => {
                     synchronize: true,
                 }),
                 TypeOrmModule.forFeature([Compound]),
+                JwtModule.registerAsync({
+                    imports: [ConfigModule],
+                    inject: [ConfigService],
+                    useFactory: (configService: ConfigService) => ({
+                        global: true,
+                        secret: configService.get<string>('JWT_SECRET'),
+                    }),
+                }),
             ],
             controllers: [CompoundController],
             providers: [CompoundService],
@@ -30,6 +45,8 @@ describe('CompoundController', () => {
         app = module.createNestApplication();
         await app.init();
         repository = module.get<Repository<Compound>>(getRepositoryToken(Compound));
+        jwtService = module.get<JwtService>(JwtService);
+        mockToken = jwtService.sign({ sub: "user-123", roles: ['Admin'] });
     });
 
     beforeEach(async () => {
@@ -54,6 +71,31 @@ describe('CompoundController', () => {
                 expect(Array.isArray(body)).toBe(true);
                 expect(body.length).toBe(1);
                 expect(body[0].title).toBe('Paracetamol');
+            });
+    });
+
+    it('should return one compound', () => {
+        return request(app.getHttpServer() as App)
+            .get('/api/compounds/1')
+            .set("Authorization", `Bearer ${mockToken}`)
+            .expect(200)
+            .expect((res) => {
+                const compound = res.body as Compound;
+                expect(compound).toBeDefined();
+                expect(compound.title).toBe('Paracetamol');
+            });
+    });
+
+    it('should create one compound', () => {
+        return request(app.getHttpServer() as App)
+            .post('/api/compounds')
+            .set('Authorization', `Bearer ${mockToken}`)
+            .send({ title: "Ibuprofen", unit: "mg", min: "200", max: "400", active: true })
+            .expect(201)
+            .expect((res) => {
+                const compound = res.body as Compound;
+                expect(compound).toBeDefined();
+                expect(compound.title).toBe('Ibuprofen');
             });
     });
 });
