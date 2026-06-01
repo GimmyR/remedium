54ae04448b98193eefbac349a681fe0e5682aaa2
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-06-01 11:43:12 +0300

    Create TestsListPage component to display a page with all compounds tests and make unit test for it

---
+++ front/src/app/admin/tests/page.tsx
@@ -0,0 +1,19 @@
+import { verifyAdminAuth } from "@/actions/authentication";
+import { fetchAllCompoundsTests } from "@/actions/compounds-test";
+import TestsTable from "@/components/tests-table";
+
+export default async function TestsListPage() {
+    await verifyAdminAuth();
+    const tests = await fetchAllCompoundsTests();
+
+    return (
+        <main className="min-vh-100 bg-light pt-5 position-relative">
+            <div className="d-flex flex-column align-items-center pt-5">
+                <h1 className="fs-2 fw-bold text-center mt-lg-3">List of all tests</h1>
+                <div className="col-12 col-xl-5 col-xxl-4 mt-5 mb-5 pb-5">
+                    <TestsTable tests={tests}/>
+                </div>
+            </div>
+        </main>
+    );
+}
---
+++ front/src/app/admin/tests/tests.test.tsx
@@ -0,0 +1,43 @@
+import { render, screen } from "@testing-library/react";
+import { format } from "date-fns";
+import TestsListPage from "./page";
+
+const tests = [
+    {
+        "id": 1,
+        "testDate": new Date(),
+        "details": [
+            {
+                "id": 2,
+                "compound": {
+                    "id": 1,
+                    "title": "Paracetamol",
+                    "unit": "mg",
+                    "min": 500,
+                    "max": 1000,
+                    "active": true
+                },
+                "amount": 300
+            }
+        ]
+    }
+];
+
+jest.mock("@/actions/authentication", () => ({
+    verifyAdminAuth: jest.fn()
+}));
+
+jest.mock("@/actions/compounds-test", () => ({
+    fetchAllCompoundsTests: jest.fn(() => Promise.resolve(tests))
+}));
+
+describe("Test TestsTable component", () => {
+    it("Should display 'List of all tests' title and a list of compounds test", async () => {
+        const Tests = await (TestsListPage() as any);
+        render(Tests);
+        const title = screen.getByText("List of all tests");
+        expect(title).toBeInTheDocument();
+        const date = screen.getByText(format(tests[0].testDate, "dd/MM/yyyy"));
+        expect(date).toBeInTheDocument();
+    });
+});
