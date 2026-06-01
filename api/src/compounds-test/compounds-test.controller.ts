63f5c55f7a3ba46cd77df787aaee82379ae19ed2
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-06-01 09:28:58 +0300

    Add Swagger decorators

--- api/src/compounds-test/compounds-test.controller.ts
+++ api/src/compounds-test/compounds-test.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Get, HttpStatus, Post, UseGuards } from '@nestjs/common';
 import { CompoundTestDto } from './compound-test.dto';
 import { CompoundsTestService } from './compounds-test.service';
-import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { RolesGuard } from 'src/auth/roles.guard';
 import { Roles } from 'src/auth/roles.decorator';
 
 @ApiTags('compounds-tests')
+@ApiBearerAuth("access-token")
 @Controller('api/compounds-tests')
 export class CompoundsTestController {
     constructor(private readonly compoundsTestService: CompoundsTestService) {}
@@ -23,6 +24,9 @@ export class CompoundsTestController {
     @Get()
     @UseGuards(AuthGuard, RolesGuard)
     @Roles("Admin")
+    @ApiOperation({ summary: "Find all compounds tests" })
+    @ApiResponse({ status: HttpStatus.OK, description: "All compounds tests are found" })
+    @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: "No authenticated admin to use this endpoint correctly" })
     async findAllTests() {
         return await this.compoundsTestService.findAll();
     }
