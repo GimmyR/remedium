f2951aedb81e9c86974b5ca9f0b88239f9e292a3
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-06-03 10:13:16 +0300

    Create PUBLIC and PRIVATE URL

--- front/src/lib/urls.ts
+++ front/src/lib/urls.ts
@@ -1,1 +1,2 @@
-export const API_URL = process.env.NEXT_PUBLIC_CLIENT_SIDE_TO_API;
+export const PUBLIC_URL = process.env.NEXT_PUBLIC_CLIENT_SIDE_TO_API;
+export const PRIVATE_URL = process.env.SERVER_SIDE_TO_API;
